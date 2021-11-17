4d5c48728ee60eaece6dc40130b32a21f492b326
Luis Rodriguez <eltecladodesocrates@gmail.com>
2021-11-16 22:42:34 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  // Get all the dat in the store and then play with it in the app
+
+  const [country, setCountry] = useState('usa')
+
+  fetch(`https://covid19.mathdro.id/api/countries/india`)
+  .then( res => res.json())
+  .then( res => console.log(res.confirmed.value))
+
+  console.log(country)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Covid App</h1>
+      <input value={country} onChange={ e => setCountry(e.target.value)}/>
     </div>
-  );
+  )
 }
 
 export default App;
